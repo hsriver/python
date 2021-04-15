cca88a4e3749c3be93d003891998255c3eaffac5
takumi-hasegawa <hase12prog@gmail.com>
2021-04-15 09:02:08 +0900

    renew

--- yt-auto.py
+++ yt-auto.py
@@ -4,9 +4,11 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
 
 #
 # Seleniumをあらゆる環境で起動させるChromeオプション
@@ -16,9 +18,13 @@ options.add_argument('--disable-gpu')
 options.add_argument('--disable-extensions')
 options.add_argument('--proxy-server="direct://"')
 options.add_argument('--proxy-bypass-list=*')
+
+options = webdriver.ChromeOptions()
+profile_path = '/Users/takum/AppData/Local/Google/Chrome/User Data'
+options.add_argument('--user-data-dir=' + profile_path)
+options.add_argument('--profile-directory=Default')
 #options.add_argument('--start-maximized')
 # options.add_argument('--headless'); # ※ヘッドレスモードを使用する場合、コメントアウトを外す
-
 
 #
 # Chrome ドライバーの起動
@@ -29,29 +35,38 @@ DRIVER_PATH = '/Users/takum/Selenium/chromedriver'
 # ブラウザの起動
 driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
 
-# Webページにアクセスする
-url = 'https://google.com/'
-driver.get(url)
+# 動画にアクセスする
+while True:
+    # urlの指定
+    url = 'https://www.youtube.com/watch?v=5GiSc6BdWTE&t=2427s'
+    driver.get(url)
 
-# 検索窓にSeleniumと入力する
-selector = '#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input'
-element = driver.find_element_by_css_selector(selector)
-element.send_keys('Selenium')
+    time.sleep(5)
+    while True:
+        #ホバーしてタイマーを表示させる
+        driver_action = ActionChains(driver)
+        driver_action.move_to_element(driver.find_element_by_xpath('//*[@id="movie_player"]')).perform()
 
-# enterキーを押す
-element.send_keys(Keys.ENTER)
+        #時間を取得
+        selector = 'ytp-time-current'
+        currentTime = driver.find_element_by_class_name(selector).text
+        currentTime = currentTime.split(':')
+        print(currentTime)
+        print(currentTime[0])
+        print(currentTime[1])
+        #min = int(currentTime[0])
+        #sec = int(currentTime[1])
 
-# 1位の記事のタイトルを取得する
-selector = '#rso > div > div:nth-child(1) > div > a > h3 > span'
-element = driver.find_element_by_css_selector(selector)
-page_title = element.text
+        #print(min + " and ")
+        #print(sec)
 
-# 1位の記事のURLを取得する
-selector = '#rso > div > div:nth-child(1) > div > a'
-element = driver.find_element_by_css_selector(selector)
-page_url = element.get_attribute('href')
+        # if min >= 44 and sec >= 40:
+            # break
 
-# ブラウザを終了する(全てのウィンドウを閉じる）
-driver.quit()
+        # time.sleep(2)
+        break
 
-print(page_title, page_url)
+
+
+
+
